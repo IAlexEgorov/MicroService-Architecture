3871f97c37c3baa3682eb4121ed3db483fc36862
Alex Egorov <sasha-egorov-01@list.ru>
2021-07-29 12:43:01 +0300

    DVPS: add alerting

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import telegram, json, logging
+from dateutil import parser
 from flask import Flask
+from flask import request
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+chatID = "-553226721"
+bot = telegram.Bot(token="1915502574:AAHnc-Ugr-LdYHkWkRG-Ts7LD8GykQZSdbw")
+
 @app.route('/')
 def main():
-    return "It is page!!!"
+    return "It is page!"
+
+@app.route('/alert', methods = ['POST'])
+def postAlertmanager():
+    content = json.loads(request.get_data())
+    bot.sendMessage(chat_id=chatID, text="Start")
+    for alert in content['alerts']:
+        if alert['status'] == 'firing':
+            message = alert['status']
+        bot.sendMessage(chat_id=chatID, text=message)
+        return "Alert OK", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
